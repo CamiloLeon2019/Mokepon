663a15dc696131511416cea355a40f1324c09c47
CamiloLeon2019 <cami10leon@hotmail.com>
2023-04-06 18:13:02 +0000

    app containerized

--- js/server.js
+++ js/server.js
@@ -1,8 +1,30 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!\n');
+  const filePath = req.url === '/' ? 'index.html' : req.url.slice(1);
+  const extname = path.extname(filePath);
+  let contentType = 'text/html; charset=UTF-8';
+  
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+  }
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('File not found');
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(data);
+    }
+  });
 });
 
 server.listen(3000, () => {
