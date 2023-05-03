7d5cf9476de2c32796558c1df5418a662de309ca
Camilo Leon <cami10leon@hotmail.com>
2023-05-02 23:17:24 -0500

    servidor de node

--- js/server.js
+++ js/server.js
@@ -1,32 +1,11 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const express = require("express")
 
-const server = http.createServer((req, res) => {
-  const filePath = req.url === '/' ? 'index.html' : req.url.slice(1);
-  const extname = path.extname(filePath);
-  let contentType = 'text/html; charset=UTF-8';
-  
-  switch (extname) {
-    case '.js':
-      contentType = 'text/javascript';
-      break;
-    case '.css':
-      contentType = 'text/css';
-      break;
-  }
+const app = express()
 
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end('File not found');
-    } else {
-      res.writeHead(200, { 'Content-Type': contentType });
-      res.end(data);
-    }
-  });
-});
+app.get("/", (req,res) =>{
+    res.send("Hola")
+})
 
-server.listen(3000, () => {
-  console.log('Server listening on http://localhost:3000');
-});
+app.listen(8080, () =>{
+    console.log("Servidor Express funcionando")
+})
